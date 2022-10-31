6c93be8adc0f5a7c0cf23b579f3a6d16a4cee6de
OlhaNekrylova <nekrylovaurist@gmail.com>
2022-10-31 15:09:44 +0200

    js

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categoriesEl = document.querySelectorAll('li.item');
+console.log(categoriesEl);
+const calculateCategories = (items) => {
+    let total = 0;
+    categoriesEl.forEach((item) => (total += 1));
+    return categoriesEl.length;
+}
+console.log('Number of categories:', categoriesEl.length);
