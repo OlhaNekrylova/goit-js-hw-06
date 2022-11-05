649b993d738093474a18c66d11ba94f7b5ba15f0
OlhaNekrylova <nekrylovaurist@gmail.com>
2022-11-05 22:25:21 +0200

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -2,12 +2,31 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const collectionEl = document.querySelector('#controls');
+const divEl = document.querySelector('#controls');
 const btnCreateEl = document.querySelector('[data-create]');
 const btnDestroyEl = document.querySelector('[data-destroy]');
-const divEl = document.querySelector('#boxes');
+const collectionEl = document.querySelector('#boxes');
 console.log(collectionEl);
 
-const createBoxes (amount) {
+btnCreateEl.addEventListener('click', onbtnCreateElClick);
+function onbtnCreateElClick (event) {
 
+};
+
+btnDestroyEl.addEventListener('click', onbtnDestroyElClick);
+function onbtnDestroyElClick (event) {
+  
+};
+
+const amount = 0; 
+
+function createBoxes (amount) {
+  const newEl = document.createElement('div');
+  return newEl;
+  console.log(newEl);
 }
+  const newElements = createBoxes (amount);
+  console.log(newElements);
+
+  collectionEl.append(...newElements);
+  console.log(collectionEl);
