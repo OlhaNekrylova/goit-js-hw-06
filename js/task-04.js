2c78da0beb9313541fe460f66d8c9ea248b2ad24
OlhaNekrylova <nekrylovaurist@gmail.com>
2022-11-05 18:39:18 +0200

    task 9

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,25 @@
-const counterValue = 0;
-const counterEl = document.querySelector('#counter');
+const counterValue = {
+    value = 0,
 
+    descrement () {
+        this.value -= 1;
+    },
+
+    increment () {
+        this.value += 1; 
+    },
+};
+
+const descrementBtnEl = document.querySelector('[descrement]');
+const incrementBtnEl = document.querySelector('[increment]');
+const valueEl = document.querySelector('span');
+
+descrementBtnEl.addEventListener('click', function (event) {
+    valueEl.textContent = counterValue.value;
+    counterValue.descrement();
+});
+
+incrementBtnEl.addEventListener('click', function (event) {
+    valueEl.textContent = counterValue.value;
+    counterValue.increment();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  button: document.querySelector('.change-color'),
+  span: document.querySelector('.color'),
+};
+
+refs.button.addEventListener('click', onButtonClick);
+
+function onButtonClick (event) {
+    document.body.style.backgroundColor = getRandomHexColor();
+    refs.span.textContent = getRandomHexColor();
+    console.log(refs.span);
+}
