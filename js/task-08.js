29ffd1d30e02d25a8fa15e6a2021d1d533b4d83f
OlhaNekrylova <nekrylovaurist@gmail.com>
2022-11-05 16:12:13 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const formEl = document.querySelector('.login-form');
+const inputEl = document.querySelector('.login-form > input');
+formEl.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit (event) {
+    event.preventDefault();
+    console.log(event);
+    const {elements: { email, password}} = event.currentTarget;
+    if (email.value === ' ' || password.value === ' ') {
+        console.log(alert('Please, fill in all fields!'));
+    } 
+    
+    console.log({elements: { email, password}});
+    event.currentTarget.reset();
+}
